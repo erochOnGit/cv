c3ed468b1d73daed674ec53232b4c1c1df7b6159
emile roch <emile31.roch@gmail.com>
2017-11-18 13:44:12 +0100

    commit create react app - refs #noBranch

---
+++ cv/src/SideBar/SideBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SideBar = props => (
+  <div
+    style={{
+      width: "33vw",
+      height: "100vh",
+      overflow: "auto",
+      background: "#eee"
+    }}
+    {...props}
+  />
+);
+export default SideBar;
